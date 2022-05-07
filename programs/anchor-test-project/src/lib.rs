927bc13b18d9cdac01222961bf436c802ca6b365
nonam3e <lendedland@gmail.com>
2022-05-07 23:37:58 +0700

    hello world

--- programs/anchor-test-project/src/lib.rs
+++ programs/anchor-test-project/src/lib.rs
@@ -1,15 +1,40 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("9ex26jHoJ3mvNzHhRTBXGyjbkXQXQX28HfZW8hPC6LM7");
 
 #[program]
 pub mod anchor_test_project {
     use super::*;
+    pub fn create(ctx: Context<Create>) -> Result<()> {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.count = 0;
+        Ok(())
+    }
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.count+=1;
         Ok(())
     }
 }
 
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Create<'info> {
+    #[account(init, payer = user, space = 16 + 16)]
+    pub base_account: Account <'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program <'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut)]
+    pub base_account: Account <'info, BaseAccount>,
+}
+
+#[account]
+pub struct BaseAccount {
+    pub count: u64,
+}
