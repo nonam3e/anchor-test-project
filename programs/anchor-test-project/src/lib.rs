a55d4595b61637e027358474a24626a1ad011b93
nonam3e <lendedland@gmail.com>
2022-05-09 03:54:16 +0700

    donate uncomplited test

--- programs/anchor-test-project/src/lib.rs
+++ programs/anchor-test-project/src/lib.rs
@@ -12,32 +12,23 @@ pub mod anchor_test_project {
         let admin_account = & ctx.accounts.admin_account;
         crowd_account.owner_id = admin_account.unsigned_key().clone();
         data_account.owner_id = crowd_account.key().clone();
-        data_account.data_list.push(String::from("Success"));
+        data_account.data_list.push(String::from("Created"));
         Ok(())
     }
 
-    // pub fn donate(ctx: Context<Donate>, lamports: &[u8]) -> anchor_lang::Result<()> {
-    //     let data_account = &mut ctx.accounts.data_account;
-    //     let patron_account = &mut ctx.account.patron_account;
-    //     let crowd_account = &mut ctx.accounts.crowd_account;
-    //     let sol_transfer = anchor_lang::solana_program::system_instruction::transfer(
-    //         &ctx.accounts.patron_account.key,
-    //         &ctx.accounts.crowd_account.key,
-    //         lamports,
-    //     );
-    //     let result = anchor_lang::solana_program::program::invoke(
-    //         &sol_transfer,
-    //         &[
-    //             ctx.accounts.patron_account.clone(),
-    //             ctx.accounts.crowd_account.clone(),
-    //             ctx.accounts.system_program.clone(),
-    //         ],
-    //     )?;
-    //     if result.is_ok() {
-    //         data_account.data_list.push(format!("Account {} sent {} lamports", &patron_account.key, &lamports));
-    //     }
-    //     result
-    // }
+    pub fn donate(ctx: Context<Donate>, fn_lamports: u64) -> anchor_lang::Result<()> {
+        let data_account = &mut ctx.accounts.data_account;
+        let patron_account = &mut ctx.accounts.patron_account;
+        let crowd_account = &mut ctx.accounts.crowd_account;
+        let system_program = ctx.accounts.system_program.to_account_info();
+        let to_trans = anchor_lang::system_program::Transfer{from: patron_account.to_account_info(), to: crowd_account.to_account_info()};
+        anchor_lang::system_program::transfer(anchor_lang::context::CpiContext::new(system_program, to_trans), fn_lamports);
+
+        let output = format!("Account {} sent {} lamports", &patron_account.unsigned_key(), fn_lamports);
+
+        data_account.data_list.push(output);
+        Ok(())
+    }
     //
     // pub fn withdraw(ctx: Context<Withdraw>, lamports: &[u8]) -> anchor_lang::Result<()> {
     //     let data_account = &mut ctx.accounts.data_account;
@@ -65,7 +56,7 @@ pub mod anchor_test_project {
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-    #[account(init, payer = admin_account, space = 64 + 64)] //can I use zero instead?
+    #[account(init, payer = admin_account, space = 128 + 128)] //can I use zero instead?
     pub data_account: Account<'info, DataAccount>,
     #[account(init, payer = admin_account, space = 8 + 32)]
     pub crowd_account: Account<'info, CrowdAccount>,
@@ -75,16 +66,16 @@ pub struct Initialize<'info> {
 }
 
 
-// #[derive(Accounts)]
-// pub struct Donate<'info> {
-//     #[account(mut, owner = crowd_account)]
-//     pub data_account: Account<'info, DataAccount>,
-//     #[account(mut)]
-//     pub patron_account: Signer<'info>,
-//     #[account(mut)]
-//     pub crowd_account: AccountInfo<'info>,
-//     pub system_program: Program<'info, System>,
-// }
+#[derive(Accounts)]
+pub struct Donate<'info> {
+    #[account(mut)]
+    pub data_account: Account<'info, DataAccount>,
+    #[account(mut)]
+    pub patron_account: Signer<'info>,
+    #[account(mut)]
+    pub crowd_account: Account<'info, CrowdAccount>,
+    pub system_program: Program<'info, System>,
+}
 //
 // #[derive(Accounts)]
 // pub struct Withdraw<'info> {
--- tests/anchor-test-project.js
+++ tests/anchor-test-project.js
@@ -1,49 +1,73 @@
 const assert = require("assert");
 const anchor = require("@project-serum/anchor");
-const { SystemProgram } = anchor.web3;
+const {
+    SystemProgram
+} = anchor.web3;
 
 describe("anchor-test-project", () => {
-  /* create and set a Provider */
-  const provider = anchor.AnchorProvider.local();
-  anchor.setProvider(provider);
+    /* create and set a Provider */
+    const provider = anchor.AnchorProvider.local();
+    anchor.setProvider(provider);
 
-  it("It initializes accounts", async () => {
-      const dataAccount = anchor.web3.Keypair.generate();
-      const crowdAccount = anchor.web3.Keypair.generate();
-      const program = anchor.workspace.AnchorTestProject;
+    const program = anchor.workspace.AnchorTestProject;
 
-      await program.rpc.initialize({
-          accounts: {
-              crowdAccount: crowdAccount.publicKey,
-              adminAccount: provider.wallet.publicKey,
-              systemProgram: SystemProgram.programId,
-              dataAccount: dataAccount.publicKey,
-          },
-          signers: [crowdAccount,dataAccount],
-      });
 
-    const account = await program.account.dataAccount.fetch(dataAccount.publicKey);
-    const check = await program.account.crowdAccount.fetch(crowdAccount.publicKey)
-    console.log('All data: ', account.dataList);
-    assert.ok(account.dataList[0] === ("Success"));
-    assert.ok(check.ownerId.toString() === provider.wallet.publicKey.toString())
-    assert.ok(account.ownerId.toString() === crowdAccount.publicKey.toString());
-  });
+    const dataAccount = anchor.web3.Keypair.generate();
+    const crowdAccount = anchor.web3.Keypair.generate();
 
-  // it("Updates a previously created account", async () => {
-  //   const baseAccount = _baseAccount;
-  //
-  //   await program.rpc.update("Some new data", {
-  //     accounts: {
-  //       baseAccount: baseAccount.publicKey,
-  //     },
-  //   });
-  //
-  //   const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
-  //   console.log('Updated data: ', account.data)
-  //   assert.ok(account.data === "Some new data");
-  //   console.log('all account data:', account)
-  //   console.log('All data: ', account.dataList);
-  //   assert.ok(account.dataList.length === 2);
-  // });
+    it("It initializes accounts", async () => {
+
+        await program.rpc.initialize({
+            accounts: {
+                crowdAccount: crowdAccount.publicKey,
+                adminAccount: provider.wallet.publicKey,
+                systemProgram: SystemProgram.programId,
+                dataAccount: dataAccount.publicKey,
+            },
+            signers: [crowdAccount, dataAccount],
+        });
+
+        const account = await program.account.dataAccount.fetch(dataAccount.publicKey);
+        const check = await program.account.crowdAccount.fetch(crowdAccount.publicKey);
+        console.log('All data: ', account.dataList);
+        assert.ok(account.dataList[0] === ("Created"));
+        assert.ok(check.ownerId.toString() === provider.wallet.publicKey.toString());
+        assert.ok(account.ownerId.toString() === crowdAccount.publicKey.toString());
+        _crowdAccount = crowdAccount;
+        _dataAccount = dataAccount;
+        _lamports = check.lamports;
+    });
+
+    it("transfer lamports", async () => {
+
+
+        await program.rpc.donate(new anchor.BN(1234), {
+            accounts: {
+                crowdAccount: crowdAccount.publicKey,
+                patronAccount: provider.wallet.publicKey,
+                dataAccount: dataAccount.publicKey,
+                systemProgram: SystemProgram.programId,
+            },
+        });
+        assert.ok(true);
+        // const checkpatr = await program.account.patronAccount.fetch(patronAccount.publicKey);
+
+    });
+
+    // it("Updates a previously created account", async () => {
+    //   const baseAccount = _baseAccount;
+    //
+    //   await program.rpc.update("Some new data", {
+    //     accounts: {
+    //       baseAccount: baseAccount.publicKey,
+    //     },
+    //   });
+    //
+    //   const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    //   console.log('Updated data: ', account.data)
+    //   assert.ok(account.data === "Some new data");
+    //   console.log('all account data:', account)
+    //   console.log('All data: ', account.dataList);
+    //   assert.ok(account.dataList.length === 2);
+    // });
 });
